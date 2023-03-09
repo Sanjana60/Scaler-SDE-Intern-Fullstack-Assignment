fd4099cda352a91cafdb9e77a27c20a6a796db03
Sanjana60 <sanjanasingla23@gmail.com>
2023-03-10 01:34:54 +0530

    Search Bar done

--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import './Searchbar.css'
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineSearch,AiOutlineSortDescending } from 'react-icons/ai';
+
+import Dropdown from 'react-dropdown';
 
 function SearchBar() {
+
+    const options = [
+        'Relevance', 'Price', 'Rating'
+        ];
+
+    const defaultOption = options[0];
+    
   return (
     <>
         <div className="searchbar">
-            <input placeholder="Search" />
-            <AiOutlineSearch style={{paddingRight:"30px"}}/>
+            <input placeholder="Search" style={{color:"#474747"}} />
+            <AiOutlineSearch style={{paddingRight:"30px",color:"#474747"}}/>
         </div>
-        <hr style={{margin:"0px 30px"}}/>
+        <hr style={{margin:"0px 30px",border: "1px solid #999999"}}/>
+        <div className="searchbar_sort">
+            <p>Sort by :</p>
+            <Dropdown options={options} value={defaultOption} placeholder="Relevance" />;
+            <AiOutlineSortDescending size={17} style={{color:"#474747"}}/>
+
+        </div>
+        
     </>
     
 
