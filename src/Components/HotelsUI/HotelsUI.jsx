5dccfe759c2240893e5e338786cde9d28bfca0cb
Sanjana60 <sanjanasingla23@gmail.com>
2023-03-10 21:03:58 +0530

    fixed

--- src/Components/HotelsUI/HotelsUI.jsx
+++ src/Components/HotelsUI/HotelsUI.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import './HotelsUI.css'
 import { useState } from 'react'
 import { FaBed ,FaShower,FaHome} from 'react-icons/fa';
+import { IoIosArrowBack} from 'react-icons/io';
 import Rating from "../../widgets/Rating"
+
+
+// import 'react-times/css/material/default.css';
+// or you can use classic theme
+// import 'react-times/css/classic/default.css';
+
 
 function HotelsUI() {
     const names = ['Bruce', 'Clark', 'Diana','Bruce', 'Clark', 'Diana','Bruce', 'Clark', 'Diana']
-    const names2 = ['Bruce', 'Clark', 'Diana']
+    const types = ['Type A', 'Type B', 'Type C']
     const [showHotelBooking, setshowHotelBooking] = useState(false);
+
 
   
   
@@ -25,8 +33,13 @@ function HotelsUI() {
         {
             (!showHotelBooking)
             ? <div className="hotelsui">{names.map((name,i) => <HotelCard key={i} handleClick={showHotelBook}/>)}</div>
-            : <div className="hotelsui">{names2.map((name,i) => <BookingHotel key={i} handleClick={dontshowHotelBook}/>)}</div>
-            // : <div className="hotelsui"><BookingHotel handleClick={dontshowHotelBook}/></div>
+            : <div>
+                <div onClick={dontshowHotelBook} className="hotelsui_backarrow">
+                    <IoIosArrowBack/>
+                    <p>Back to Home Screen</p>
+                </div>
+                <div className="hotelsui">{types.map((name,i) => <BookingHotel key={i} selected={name}/>)}</div>
+            </div>
         }
     </>
 
@@ -66,33 +79,47 @@ function HotelCard({handleClick}){
 
 
 
-function BookingHotel({handleClick}){
+function BookingHotel(props){
+
+    const [value, setValue] = useState('10:00');
+
     return (
-        <div className="hotelbookingcard" onClick={handleClick}>
-            <img  src="https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWxzfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="hii" />
+        <div className="hotelbookingcard" style={{height:"200px"}} onClick={()=>{console.log(props.selected)}}>
+            <div className="hotelbookingcard_column1">
+                <img  src="https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWxzfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="hii" />
+                <div className="hotelbookingcard_right">
+                    <h4>{props.selected}</h4>
+                    <Rating rate={4}/>
+                    <p>Nitikan Umbu</p>
+                    <h4 id='bookingprice'>&#8377; 5000</h4>
+                    <div className="hotelbookingcard_icons">
+                        <div className="hotelcard_icons_row">
+                            <FaBed style={{paddingRight:"3px",color:"#474747"}}/>
+                            <p>2</p>
+                        </div>
+                        <div className="hotelbookingcard_icons_row">
+                            <FaShower style={{paddingRight:"3px",color:"#474747"}}/>
+                            <p>2</p>
+                        </div>
+                        <div className="hotelbookingcard_icons_row">
+                            <FaHome style={{paddingRight:"3px",color:"#474747"}}/>
+                            <p>200m<sup>2</sup></p>
+                        </div>
+                    </div>
 
-            <div className="hotelbookingcard_right">
-                <h4>Type A</h4>
-                <Rating rate={4}/>
-                <p>Nitikan Umbu</p>
-                <h4 id='bookingprice'>&#8377; 5000</h4>
-                <div className="hotelbookingcard_icons">
-                    <div className="hotelcard_icons_row">
-                        <FaBed style={{paddingRight:"3px",color:"#474747"}}/>
-                        <p>2</p>
-                    </div>
-                    <div className="hotelbookingcard_icons_row">
-                        <FaShower style={{paddingRight:"3px",color:"#474747"}}/>
-                        <p>2</p>
-                    </div>
-                    <div className="hotelbookingcard_icons_row">
-                        <FaHome style={{paddingRight:"3px",color:"#474747"}}/>
-                        <p>200m<sup>2</sup></p>
-                    </div>
                 </div>
-
+            </div>
+            <h4>Details</h4>
+            <div className="hotelbookingcard_input"> 
+                <input type="text" placeholder='Email ID'></input>
+                <input type="number" placeholder='Room Number'></input>
+            </div>
+            <div className="hotelbookingcard_input"> 
+                <input type="number" placeholder='Room Number'></input>
             </div>
             
+
+
             
             
         </div>
