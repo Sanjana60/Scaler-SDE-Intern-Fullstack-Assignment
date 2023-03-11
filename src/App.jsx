20a9be6b4446a60652fc17b8b364679a13c2149f
Sanjana60 <sanjanasingla23@gmail.com>
2023-03-11 05:36:21 +0530

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,68 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Navbar from './Components/Navbar/Navbar'
 import Searchbar from './Components/Searchbar/Searchbar'
 import HotelsUI from './Components/HotelsUI/HotelsUI'
+import CircularIcon from "./assets/CircularIcon.jpeg"
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 
 function App() {
 
   return (
     <>
-      <div className="LeftSide">
-        <Navbar/>
-        <Searchbar/>
-        <HotelsUI/>
+      <Router>
+          <div className="LeftSide">
+            <div className="row">
+              <div className="row_left">
+                  <Link to="/" style={{textDecoration:"none"}}><h3>Hotel</h3></Link>
+                  <Link to="/booked" style={{textDecoration:"none"}}><p>Booked</p></Link>
+              </div>
+              <div className="row_right">
+                  <img src={CircularIcon} />
+              </div>
+            </div>
+            <Searchbar/>
+            <Routes>
+            <Route path="/" element={<Hotel />} />
+            <Route path="/booked" element={<Booked />} />
+          </Routes>
+          </div>
+          
 
-      </div>
+
+      </Router>
+     
     </>
   )
 }
+
+
+function Hotel() {
+
+  return (
+    <>
+        <HotelsUI/>
+    </>
+  )
+}
+
+function Booked() {
+
+  return (
+    <>
+        <div>hii</div>
+    </>
+  )
+}
+
+
 
 export default App
